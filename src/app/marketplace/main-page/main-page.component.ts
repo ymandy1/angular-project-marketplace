3b87d35c2e17222cfc5e88cd8102aa4665aff3bc
Amanda Rodrigues <amanda.study110@gmail.com>
2024-06-17 20:52:05 -0300

    add sweet alert

--- src/app/marketplace/main-page/main-page.component.ts
+++ src/app/marketplace/main-page/main-page.component.ts
@@ -1,34 +1,38 @@
-import { Component } from '@angular/core';
-import { PlantListComponent } from '../plant-list/plant-list.component';
+import { Component, OnInit } from '@angular/core';
 import { ProdutoService } from '../../vender-produto/service/produto.service';
 import { CarrinhoService } from '../../marketplace/carrinho/service/carrinho.service';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.css'
+  styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent {
-
-  constructor(private produtoService: ProdutoService, private carrinhoService: CarrinhoService, private db: AngularFireDatabase) { }
+export class MainPageComponent implements OnInit {
 
   plants: any[] = [];
   userId: any;
 
+  constructor(private produtoService: ProdutoService, private carrinhoService: CarrinhoService, private db: AngularFireDatabase) { }
+
   ngOnInit(): void {
-
     this.userId = localStorage.getItem('user');
-
     this.produtoService.listar().subscribe(plantsReturn => {
       console.log(plantsReturn);
       this.plants = plantsReturn;
-    })
-
+    });
   }
 
   adicionarAoCarrinho(plant: any) {
     this.carrinhoService.adicionarProduto(this.userId, plant);
+    Swal.fire({
+      icon: 'success',
+      title: 'Produto Adicionado!',
+      text: `${plant.nome} foi adicionado ao carrinho.`,
+      showConfirmButton: false,
+      timer: 1500
+    });
   }
 
 }
